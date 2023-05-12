5436653cb8a6bbb68dbc1292f24f66ebbc24981d
Romeo <tf00117345@gmail.com>
2023-05-12 19:53:08 +0800

    fix: dev server not rewriting the path includes a dot

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import pluginRewriteAll from 'vite-plugin-rewrite-all';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
-	base: '/clinical-decision-support-app/',
+	plugins: [react(), pluginRewriteAll()],
+	base: '/clinical-decision-support-app',
 });
